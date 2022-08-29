64209536efb0f68f1010d0e147b28dd91a27258e
FernanduCastro <nandu.castro@gmail.com>
2022-08-29 19:56:54 -0300

    resolvendo o lab01
    resolução do lab01 para entender das ferramentas

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,7 +1,7 @@
 ###################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 1 - Aritmética com Inteiros
-# Nome: 
+# Nome: fernando castro
 # RA: 
 ###################################################
 
@@ -9,10 +9,10 @@ a = int(input())
 b = int(input())
 
 print("a =", a)
-print("b  =", b)
+print("b =", b)
 print("a + b =", a + b)
 print("a - b =", b - a)
 print("a * b =", a * b)
 print("a ** b =", a ** b)
-print("a % b =", a % b)
-print("a // b =", a // b)
+print("a // b =", a % b)
+print("a % b =", a // b)
