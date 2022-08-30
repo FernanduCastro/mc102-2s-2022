2f3e221a6c66dd1049f514fdc3552f5d86e49aa8
FernanduCastro <nandu.castro@gmail.com>
2022-08-30 18:47:52 -0300

    Retirado print dos inputs

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -6,11 +6,11 @@
 ###################################################
 
 # Leitura de dados
-D1_SpaceX = int(input('Distância da trajetória, em quilômetros (km), entre a Terra e Marte no lançamento da SpaceX: '))
-V1_SpaceX = int(input('Velocidade da espaçonave da SpaceX em quilômetros por hora (km/h): '))
-Time_pass = int(input('Tempo passado, em dias, entre o lançamento da SpaceX e Blue Origin: '))
-D2_BOrigin = int(input('Distância da trajetória, em quilômetros (km), entre a Terra e Marte no lançamento da Blue Origin: '))
-V2_BOrigin = int(input('Velocidade da espaçonave da Blue Origin em quilômetros por hora (km/h): '))
+D1_SpaceX = int(input())
+V1_SpaceX = int(input())
+Time_pass = int(input())
+D2_BOrigin = int(input())
+V2_BOrigin = int(input())
 
 # Cálculo do tempo total gasto por cada espaçonave 
 calcular_spaceX = D1_SpaceX / V1_SpaceX
