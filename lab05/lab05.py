aa48f9da922c345303e1e06ff0268115d24d631d
FernanduCastro <nandu.castro@gmail.com>
2022-09-26 02:09:42 -0300

    Finalizado Teste do Exercício lab05

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -1,23 +1,36 @@
 ##################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 5 - Jornada de Trabalho
-# Nome:
-# RA:
+# Nome: FERNANDO CASTRO
+# RA: DONT SEI QUE PORRA IS THIS!!
 ##################################################
 
 # Leitura do valor da hora
-V = int(input())
+valorHora = int(input())
 
 # Leitura do numero de dias trabalhados na semana
-D = int(input())
+numeroDias = int(input())
 
 # Leitora e processamento dos periodos de trabalho de cada dia
+horas_trabalhadas = 0
+horas_extras = 0
+for numero in range(0,numeroDias):
+    calcularHoraExtra = 0
+    numeroPeriodos = int(input())
+    for num in range(0, numeroPeriodos):
+        inicioPeriodo = int(input())
+        fimPeriodo = int(input())
+        calcularHoraExtra +=  fimPeriodo - inicioPeriodo
 
-
+    if calcularHoraExtra > 8:
+        horas_extras += (calcularHoraExtra - 8)
+    horas_trabalhadas += calcularHoraExtra
 
 # Calculo do valor devido ao funcionário
 
-
+if (horas_trabalhadas - horas_extras) > 44:
+    horas_extras += ((horas_trabalhadas - 44 ) - horas_extras)
+valor = ( horas_trabalhadas * valorHora ) + ((horas_extras * valorHora) * 0.5)
 
 # Impressão da saída
 print("Horas trabalhadas:", horas_trabalhadas)
