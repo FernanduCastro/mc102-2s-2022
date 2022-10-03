b2e590e15309a8ae8cd0f3144b8256f9e19dd559
Helen Koike <helen@koikeco.de>
2022-10-03 20:37:08 -0300

    Adding lab07 files and ci tests

---
+++ lab07/lab07.py
@@ -0,0 +1,21 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 7 - Disconnect
+# Nome:
+# RA:
+###################################################
+
+# Leitura das tropas de defesa
+
+
+# Leitura das tropas de ataque
+
+
+# Processamento da guerra
+
+
+# Saída de dados
+# ...
+  print('Derrota')
+# ...
+  print('Vitória posicionando as tropas a partir da posição', posicao)
