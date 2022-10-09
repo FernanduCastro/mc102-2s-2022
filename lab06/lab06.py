4b965756dcded9995e2fbfd3309414bcb8008cfe
FernanduCastro <nandu.castro@gmail.com>
2022-10-09 20:37:21 -0300

    Exercício lab06 finalizado

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -1,14 +1,28 @@
 ##################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 6 - Torre de Panquecas
-# Nome:
-# RA:
+# Nome: FERNANDO CASTRO
+# RA: AGAIN, NÃO SEI!!
 ##################################################
 
 # Leitura da torre de panquecas
 torre = [int(panqueca) for panqueca in input().split()]
+confirmar = torre[:]
+confirmar.sort()
 
 # Leitura e processamento dos movimentos
+while True:
+
+    espatulada = int(input())
+
+    if espatulada == 0:
+        break
+
+    torre[:espatulada] = torre[espatulada-1::-1]
 
 
 # Impressão da saída
+if torre == confirmar:
+    print('Torre estavel')
+else:
+    print('Torre instavel')
